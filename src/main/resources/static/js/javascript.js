1ca5b80c4479b5a9a18120db32bca79cb3787bc8
Prism17 <2839296425@qq.com>
2019-05-22 09:32:59 +0800

    实现通过url获取识别结果

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,3 +1,5 @@
+// 我记得黄强说你们代码规范要求高
+// 我写的时候只想着出效果 你改下吧
 var is_url=false;
 function search_url(){
 	var url=document.getElementById("search_url").value.replace(/(^\s*)|(\s*$)/g, '');
@@ -11,25 +13,47 @@ function search_url(){
 		but.setAttribute("style","border-color: #606060;background-color: rgba(0,0,0,.45);color: black;");
 	}
 }
+
+
 function search_but(){
 	if(is_url){
 		document.getElementById("background").style.backgroundImage="url("+document.getElementById("search_url").value+")";
+		var picUrl = document.getElementById("search_url").value;
+		var formData = new FormData();
+		formData.append("picUrl", picUrl);
+
+		$.ajax({
+			"url": "detect_url",
+			"type": "POST",
+			"data": formData,
+			contentType: false,
+			processData: false,
+			"success": function (data) {
+				alert(data);
+			},
+			error: function () {
+				alert("error");
+			}
+		});
+
 	}
 }
+
 function search_local(){
 	document.getElementById("background").style.backgroundImage="url("+window.URL.createObjectURL(document.getElementById("search_local").files.item(0))+")";
 	var file = document.getElementById("search_local").files[0];
-	var fromDada = new FormData();
-	fromDada.append("file", file);
+	var formData = new FormData();
+	formData.append("file", file);
 
 	$.ajax({
 		"url": "/picture",
 		"type": "POST",
-		"data": fromDada,
+		"data": formData,
 		contentType: false,
 		processData: false,
-		"success": function () {
-			alert("ok");
+		"success": function (data) {
+			//把某个span换成这个data
+			alert(data);
 		},
 		error: function () {
 			alert("error");
